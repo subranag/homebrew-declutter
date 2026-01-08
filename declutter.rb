e2f55fe9f45410e22dbe62adc4ac14119c7f23fb
subbu-mac <subbu-mac@subbu-macs-MacBook-Pro.local>
2026-01-07 21:17:17 -0800

    first release

--- declutter.rb
+++ declutter.rb
@@ -1,25 +1,25 @@
 class Declutter < Formula
   desc "Declutter web pages and turn them into archival documents"
   homepage "https://github.com/subranag/declutter"
-  version "1.0.0"
+  version "0.0.1"
 
   on_macos do
     if Hardware::CPU.intel?
       url "https://github.com/subranag/declutter/releases/download/v0.0.1/declutter-macos-x64.tar.gz"
-      sha256 "8752caa4f9b0be34723c1aec37bb6f165027974b94cd1fc641a337c34a1a36d1"
+      sha256 "1f2ea919e1d589f949136ef2582c0e4ab1fe51a632d268f0d54e56c7f2cb9243"
     elsif Hardware::CPU.arm?
       url "https://github.com/subranag/declutter/releases/download/v0.0.1/declutter-macos-arm64.tar.gz"
-      sha256 "53c0bac747c3f488aaa71e13c78228df8d2a0fe1a44bebcbe8525acd608ff1dd"
+      sha256 "6c67384507048c39f3e674d419abfd7f17ce071a25d9759b5c9761f7abed9876"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
       url "https://github.com/subranag/declutter/releases/download/v0.0.1/declutter-linux-x64.tar.gz"
-      sha256 "df96fdc94f2ae0c3df3f827bea29949d82b3048115377e1084d44a411a56134f"
+      sha256 "5eff798dd0b1e376cb232c11d9409330fffc71de334d6ef81d9c937f298f423d"
     elsif Hardware::CPU.arm?
       url "https://github.com/subranag/declutter/releases/download/v0.0.1/declutter-linux-arm64.tar.gz"
-      sha256 "81d2fa636173613ad496a79858d40d0fd7ba2413610f42612f105635b4b3bb86"
+      sha256 "38e6d962d00da3db189f37155039e2e90c570c3eeffb2e3d5b7dd4d55c130bee"
     end
   end
 
